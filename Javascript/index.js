aef6064e081d4f1933970b4a7452af23e252490f
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2020-12-19 00:17:45 +0530

    practice

---
+++ Javascript/index.js
@@ -0,0 +1,80 @@
+// 1. number datatype
+var x=5
+var y=6
+// console.log(x);
+//console.error( "it's an error");
+//console.warn( "warning");
+
+// 2. String datatype
+var str1="this is string";
+
+var mark = {
+    Alok:35,
+    vikram:65,
+    kishor:75,
+}
+
+// 3.bollen datatype:
+var a= true;
+var b=false;
+//console.log(b);
+
+// 4.undefined datatype:
+var und=undefined
+//console.log(und);
+
+// 5.null datatype:
+var n= null
+//console.log(n);
+
+// 6. array datatype:
+var arr=[1,2,3,4,5];
+//console.log (arr[2]);
+
+// operation in java script------------------
+//1. arithimatic operation:
+
+var a= 100
+var b=10
+//console.log("the value of a+b is",a+b);
+//console.log("the value of a-b is",a-b);
+//console.log("the value of a*b is",a*b);
+//console.log("the value of a/b is",a/b);
+
+//2. assignment operetor:
+
+var a=100
+var b=10
+var c= b
+c +=2
+//console.log(c);
+
+// 3. comparision operator:
+
+var x= 34
+var y=64
+//console.log(x==y);
+//console.log(x<=y);
+//console.log(x>=y);
+//console.log(x>y);
+//console.log(x<y);
+
+//4. logical operator:
+// AND
+/*
+console.log(true&&false);
+console.log(true&&true);
+console.log(false&&false);
+console.log(false&&true);
+
+// OR
+
+console.log(true||false);
+console.log(true||true);
+console.log(false||false);
+console.log(false||true);
+*/
+// NOT
+
+//console.log(!false);
+//console.log(!true);
