aec78a7b52529bcbe69448c9519ed269b5481bec
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-07 19:21:04 +0530

    array in javascript

--- Javascript/array/create.js
+++ Javascript/array/create.js
@@ -1,6 +1,7 @@
 let x=[30,32,34,36,38,40]
 console.log(x[3]);   //access specific position's array value.
 console.log(x[x.length-1]) // if u want to print last value of the array
-console.log(x)  //print the array value
 x[0]=28  //update new value to array
-console.log(x)
+console.log(x)  //print the array value
+console.log(typeof x) //to find out the type of aaray
+console.log(x.length)  //to find out the lenth of the array
