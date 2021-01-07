5ae36913b8a5caa8f4e3d92f9ec7950e089d6db9
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-07 20:12:39 +0530

    add and delete in array

---
+++ Javascript/array/addNdel.js
@@ -0,0 +1,8 @@
+let x=[1,2,"three",{},'five',6]
+
+x.pop(); // to remove last element from the array
+x.shift(); // to remove 1st element from the array
+x.push('hello') // to add a new element in the end of the array
+x.unshift("hi")  // to add a new element in the beginning of the array
+
+console.log(x);  //to print
