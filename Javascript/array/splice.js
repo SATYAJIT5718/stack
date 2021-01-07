5d8117060881294ed85cc371f3cac44691c12a20
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-07 21:53:03 +0530

    ADD AND REMOVE specific value from array

--- Javascript/array/splice.js
+++ Javascript/array/splice.js
@@ -2,3 +2,5 @@ let x=[1,2,'three',4,'five',6,7,'eight']
 console.log(x);
 x.splice(8,2,3)
 console.log(x);
+
+//The splice() method adds/removes items to/from an array, and returns the removed item(s).
