3a5aa3c7655b5ec2b8d59259b25dca17b41b6461
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-07 19:17:36 +0530

    update

---
+++ Javascript/array/create.js
@@ -0,0 +1,6 @@
+let x=[30,32,34,36,38,40]
+console.log(x[3]);   //access specific position's array value.
+console.log(x[x.length-1]) // if u want to print last value of the array
+console.log(x)  //print the array value
+x[0]=28  //update new value to array
+console.log(x)
