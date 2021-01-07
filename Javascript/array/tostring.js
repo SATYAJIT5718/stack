b8c17e09d7093cf37437887bb9300a31a4a44613
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-07 21:51:51 +0530

    BY COMMA

--- Javascript/array/tostring.js
+++ Javascript/array/tostring.js
@@ -2,3 +2,5 @@ var fruits = ["Banana", "Orange", "Apple", "Mango"];
 console.log(fruits);
   var x = fruits.toString();
   console.log(x);
+
+  // The toString() method returns a string with all the array values, separated by COMMAS.
