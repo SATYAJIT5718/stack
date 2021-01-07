2e767e46185ea6a7dbd9900b568aacc44b74a3f9
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-07 21:08:04 +0530

    sort the value of array

--- Javascript/array/sort.js
+++ Javascript/array/sort.js
@@ -1,6 +1,8 @@
-let x=[3,4,100,6,22]
-x.sort();
-console.log(x);
+// let x=[3,4,100,6,22,61]
+let y = ["Banana", "Orange", "Apple", "Mango"];
+y.sort();
+console.log(y);
 
 
 // it sort the value either ascending (up) or descending (down)
+// in no system it incorrect becoz["2" is bigger than "1" that's why"25" is bigger than "100", ]
