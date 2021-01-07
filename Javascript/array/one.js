4675371c74e3dc2e82b3b310de4b03a515d67073
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-07 19:38:43 +0530

    array

---
+++ Javascript/array/one.js
@@ -0,0 +1,9 @@
+let x=[20,40,"sixty",{},80,[1,2,3],100]
+
+for(let i=0;i<=x.length-1;i++){
+    console.log(x[i]);
+
+}
+console.log(x[5]);
+x[3]=70;
+console.log(x);
---
+++ Javascript/object/OBJECT.js
@@ -0,0 +1,14 @@
+let product = {
+    name: "HP Laptop",
+    price: 40000,
+    color: "silver",
+    status: function () {
+      console.log("Out for Delivery...");
+      return "";
+    },
+  };
+//    console.log(product);
+//    console.log(product.name);
+//    console.log(product.price);
+//    console.log(product.color);
+   console.log(product.status());
