9257f4bbf004da1125cda2c0f2606c244958501a
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-07 20:36:58 +0530

    create new array by calling function

---
+++ Javascript/array/map().js
@@ -0,0 +1,5 @@
+var numbers = [4, 9, 16, 25];
+var x = numbers.map(Math.sqrt)
+console.log(x);
+
+//The map() method creates a new array with the results of calling a function for every array element.
