8c4903fdf35cfff94f95394646ce1e92563b17c1
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-12 11:37:27 +0530

    diff btwn x++ and ++x

---
+++ Javascript/object/x++n++x.js
@@ -0,0 +1,8 @@
+let x=10;
+let a=++x;
+let b=x++;
+console.log(a+b);
+
+// if symbol come 1st then it ll update the value before going to next(++x)
+// if value comes 1st then the value remain same as mention(x++)
+// otherwise (x++ = ++x) both value are same.
