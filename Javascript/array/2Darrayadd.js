106f82389cfc1d2ac5e586020a0b5674b2d5c9b3
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-13 10:57:53 +0530

    2D array sum

---
+++ Javascript/array/2Darrayadd.js
@@ -0,0 +1,12 @@
+let twoD =[
+    [30,40],
+    [50,60],
+    [70,80],
+]
+
+// console.log(twoD.length);
+for(let i=0;i<=twoD.length-1;i++){
+    for(let j=0;j<=1;j++){
+        console.log(twoD[i][j]);
+    }
+}
