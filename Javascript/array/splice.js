50c555b6c49a6fca06b961f0dcb3a1724ddba841
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-13 16:44:43 +0530

    splice

--- Javascript/array/splice.js
+++ Javascript/array/splice.js
@@ -4,3 +4,4 @@ x.splice(8,2,3)
 console.log(x);
 
 //The splice() method adds/removes items to/from an array, and returns the removed item(s).
+//the 2nd value is for deleteing the no of value at the position of adding new object.
