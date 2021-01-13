34bd31f6fc7056aa0da54698144eb82bf2f90183
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-13 16:40:12 +0530

    update

--- Javascript/object/arraywithobj.js
+++ Javascript/object/arraywithobj.js
@@ -5,14 +5,14 @@ let product=[
     {p_name:'5130-xpress music',size:6.2,brand:'nokia',price:6000,color:'red'},
     {p_name:'zuk-z1',size:6.1,brand:'lenevo',price:15000,color:'blue'},
 ]
-// console.log(product.length);  // to find out the length of the array
-// console.log(product);
-// console.log(product[2]); //to print indivisual object in an array
+console.log(product.length);  // to find out the length of the array
+console.log(product);
+console.log(product[2]); //to print indivisual object in an array
 
-// // by specific condition how to print specifi part of the key and value
-// for(let i=0;i<=product.length-1;i++){
-//     if(product[i].color=='red')
-//     console.log('brand:',product[i].brand,'p_name:',product[i].p_name);
-// }
+// by specific condition how to print specifi part of the key and value
+for(let i=0;i<=product.length-1;i++){
+    if(product[i].color=='red')
+    console.log('brand:',product[i].brand,'p_name:',product[i].p_name);
+}
 
 console.log(product.color); // it's a undefined task becoz this array contain multiple object.
