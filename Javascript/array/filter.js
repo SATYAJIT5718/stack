fc18cfeb70c05a0b8268f6539b608446339b1c83
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-19 00:11:06 +0530

    filter

--- Javascript/array/filter.js
+++ Javascript/array/filter.js
@@ -3,5 +3,5 @@ function Adult(age){
     
     return age>=18;
 }
-console.log(ages.filter(Adult));
+console.log(ages.filter(Adult));// output is the no which is >= 18 will come.
 console.log(Adult());  // it show condition is false due to the 1st condition is false
