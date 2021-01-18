b2735a3c123ec40c19e22066cb2af9d37737a10c
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-19 00:26:28 +0530

    array find

---
+++ Javascript/array/find.js
@@ -0,0 +1,6 @@
+let x=[10,5,18,22,16,29,36,40]
+function major(adult){
+    return adult>=18
+}
+console.log(x.find(major)); // it find out the the true value as per the condition from the 1st value of array.
+
