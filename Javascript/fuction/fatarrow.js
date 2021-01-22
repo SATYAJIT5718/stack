e8ba2ca29c0de4866cf167775c3dbc2cb9856c35
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-22 10:57:55 +0530

    FAT ARROW FUNCTION IN JAVA SCRIPT

---
+++ Javascript/fuction/fatarrow.js
@@ -0,0 +1,8 @@
+function hello(){
+    console.log("hello"); // full function method
+}
+let hello=()=>'hello'; // this is a fat arrow function
+
+
+//thies two are equal.
+//diff- in case of fuction after returning the RESULT is again return UNDEFINED.... but in case of FAT ARRAOW function it only retun the value.
