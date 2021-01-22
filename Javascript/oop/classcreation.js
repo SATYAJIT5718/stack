b39a278b6b3fba8e3cb8791e28b3b44144eaf616
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-22 11:48:55 +0530

    class creation in javascript

---
+++ Javascript/oop/classcreation.js
@@ -0,0 +1,15 @@
+class student{
+    name='satya';
+    age=26;
+    location='odisha';
+    details=hello=>{
+        console.log('you are from',this.location);
+    }
+    
+}
+// console.log(new student()); 
+let x=new student();
+console.log(x.details());
+
+
+// console.log(new student());
