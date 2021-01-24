f995d07e6a61fcae36e2577949fd67cdca9fff6e
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-24 20:08:33 +0530

    constuctor in class

--- Javascript/oop/bank.js
+++ Javascript/oop/bank.js
@@ -14,12 +14,26 @@ class Union_bank_of_india{
         console.log('He is from ',this.location);
         console.log('His aacount number is ',this.acc_no);
     }
-    
 }
-let obj1=new Union_bank_of_india('satya',26,'Odisha',12345);
-let obj2=new Union_bank_of_india('iswar',25,'Karnataka',678910);
-obj1.biodata().position();
-// obj1.biodata();
-// obj2.biodata();
 
-// obj2.position().biodata();
+class type extends Union_bank_of_india{
+    constructor(name,age,address,accnumber,occupation){
+        super(name,age,address,accnumber);
+        this.cust_occupation=occupation;
+    }
+    occu(){
+        console.log(super.biodata(),'His occupation is',this.cust_occupation);
+        // console.log('His occupation is',this.cust_occupation);
+    
+    }
+}
+// let obj1=new Union_bank_of_india('satya',26,'Odisha',12345);
+// let obj2=new Union_bank_of_india('iswar',25,'Karnataka',678910);
+// obj1.biodata().position();
+    // obj1.biodata();
+// obj2.biodata();
+// obj2.position().biodata();// how to execute both method at a time?
+let obj3=new type('Lokesh',23,'JSg',16431,'Engineer');
+let obj4=new type('Jayanta',26,'Bbsr',45861,'Doctor');
+obj3.occu();
+obj4.occu();
