477fb39445b0e5adb8cc3c8a4adc36c50d993d40
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-26 10:21:44 +0530

    slice

--- Javascript/array/slice.js
+++ Javascript/array/slice.js
@@ -1,6 +1,7 @@
 let x=[1,2,3,4,5,6,7]
-let y=x.slice(1,3);
+let y=x.slice();
 console.log(y);
 
 
 //it separate the value to a new array as mention in a condition,..
+//if u give empty parantisies then whole array value ll come... 
