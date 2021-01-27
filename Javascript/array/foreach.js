eae9a796cf51b131921e621b24fa5d3db117afa3
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-27 17:07:42 +0530

    foreach

--- Javascript/array/foreach.js
+++ Javascript/array/foreach.js
@@ -1,6 +1,9 @@
 let x=["a",1,'b',2,'c',3,'d',4,'e',5]
 x.lenght;
 // console.log(x[1]);
-x.forEach(function(element,hello) {
-    console.log(hello+1,element);
+x.forEach(function(element,index) {
+    console.log(index,element);
 });
+
+//element is show the full element of the array
+//index is a name,which is use for show the position value. 
