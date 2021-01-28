d1d5bf77d5780651f4d76ea6b4fe49f43bcc57d2
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-28 10:55:25 +0530

    ES6 array supporting method

--- Javascript/array/foreach.js
+++ Javascript/array/foreach.js
@@ -1,9 +1,7 @@
 let x=["a",1,'b',2,'c',3,'d',4,'e',5]
 x.lenght;
 // console.log(x[1]);
-x.forEach(function(element,index) {
-    console.log(index,element);
-});
+x.forEach(function(x){console.log(x);});
 
 //element is show the full element of the array
 //index is a name,which is use for show the position value. 
