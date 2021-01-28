ab15eb748ee76b03f68b269047f7b868d4678de1
SATYAJIT5718 <satyajitmahakud1@gmail.com>
2021-01-28 12:06:18 +0530

    difference between var/let/const

--- Assignment/diff/let.js
+++ Assignment/diff/let.js
@@ -1,5 +1,5 @@
 let x=10;
-// let x=20;// it showing error that (Identifier 'x' has already bee declared).
+let x=20;// it showing error that (Identifier 'x' has already bee declared).
 x=20; // Now it's return the value of  x=20, due to removing LET keyword from the value.
 console.log(x);
 
